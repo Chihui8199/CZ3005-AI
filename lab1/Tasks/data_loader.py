b2565e00c76063c91dd1da56d08d8eb33de65f7a
bennnnnong <benongcm11@gmail.com>
2023-10-14 21:25:13 +0800

    task3-merged

--- lab1/Tasks/data_loader.py
+++ lab1/Tasks/data_loader.py
@@ -1,5 +1,5 @@
 import json
-from pathlib import Path
+import os
 
 _data = {
     'G': None,
@@ -8,24 +8,29 @@ _data = {
     'Coord': None
 }
 
+# Get the path to the directory where the script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Navigate to the parent directory and then into the "Data" folder
+data_dir = os.path.join(script_dir, '..', 'Data')
 
 def load_data():
     global _data
 
     if _data['G'] is None:
-        with open(Path('data') / 'G.json', 'r') as file:
+        with open(os.path.join(data_dir, 'G.json'), 'r') as file:
             _data['G'] = json.load(file)
 
     if _data['Dist'] is None:
-        with open(Path('data') / 'Dist.json', 'r') as file:
+        with open(os.path.join(data_dir, 'Dist.json'), 'r') as file:
             _data['Dist'] = json.load(file)
 
     if _data['Cost'] is None:
-        with open(Path('data') / 'Cost.json', 'r') as file:
+        with open(os.path.join(data_dir, 'Cost.json'), 'r') as file:
             _data['Cost'] = json.load(file)
 
     if _data['Coord'] is None:
-        with open(Path('data')/'Coord.json', 'r') as file:
+        with open(os.path.join(data_dir, 'Coord.json'), 'r') as file:
             _data['Coord'] = json.load(file)
 
     return _data['G'], _data['Dist'], _data['Cost'], _data['Coord']
--- lab1/main.py
+++ lab1/main.py
@@ -1,13 +1,15 @@
 import Tasks.task1 as t1
+import Tasks.task2 as t2
 
 import Tasks.task3 as t3
 
 def run():
     # load data to be used by all classes
+    energy_budget = 287932
     message = "\n\nCZ3005 Lab 1 Project Menu\n\n\
     Presets - Start Node: 1, End Node: 50, Energy Budget: 287932 == \n\
     1) UCS: Shortest Distance without Energy Constraint\n\
-    2) \n\
+    2) Refined UCS: Shortest Distance with Energy Constraint\n\
     3) A* search Algorithm \n\
     4) Quit\n"
     while True:
@@ -24,11 +26,14 @@ def run():
             t1.run_ucs()
             input("Press [Enter] to continue...")
             continue
-        elif choice == 3: 
-            print("You chose A* Search Algorithm")
-            t3.run_astar()
+        elif choice == 2:
+            print("You chose UCS: Shortest Distance with Energy Constraint")
+            t2.run_ucs_refined(energy_budget)
             input("Press [Enter] to continue...")
             continue
+        elif choice == 3: 
+            print("You chose A* Search Algorithm")
+            t3.run_astar() 
         elif choice == 4:
             print("Exiting...")
             break
